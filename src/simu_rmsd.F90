4568afb45bc54e679c3046f85e8d8ec2f6333920
naotohori <hori.naoto@gmail.com>
2016-07-18 11:29:40 -0500

    Modify output of RMSD and Rg when IONs exist

--- src/simu_rmsd.F90
+++ src/simu_rmsd.F90
@@ -12,17 +12,18 @@ subroutine simu_rmsd(rmsd_unit, rmsd)
 
   implicit none
   
-  ! -------------------------------------------------------------------
   real(PREC), intent(out) :: rmsd(:)               ! (replica)
   real(PREC), intent(out) :: rmsd_unit(:,:)        ! (unit, replica)
 
-  ! -------------------------------------------------------------------
-  ! local variables
   integer :: imp, iunit, irep, num, i1, i2
   integer :: list1(nmp_real), list2(nmp_real)
   real(PREC) :: xyz_init_mp(3, nmp_real), xyz_in_mp(3, nmp_real), xyz_out_mp(3, nmp_real)
+  logical :: flg_all
 
   ! -------------------------------------------------------------------
+
+  flg_all = .True.
+
   do imp = 1, nmp_real
      list1(imp) = imp
      list2(imp) = imp
@@ -30,18 +31,10 @@ subroutine simu_rmsd(rmsd_unit, rmsd)
 
   do irep = 1, n_replica_mpi
 
-     !i1 = 1
-     !i2 = nmp_real
-     !num = i2 - i1 + 1
-     !xyz_init_mp(1:3, i1:i2) = xyz_ref_mp(1:3, i1:i2)
-     !xyz_in_mp(1:3, i1:i2) = xyz_mp_rep(1:3, i1:i2, irep)
-     !call util_bestfit(xyz_init_mp, num, xyz_in_mp, num, num, xyz_out_mp, &
-     !     list1, list2, rmsd(irep))
-     rmsd = 0.0
-
      do iunit = 1, nunit_all
         if (iclass_unit(iunit) == CLASS%ION) then
             rmsd_unit(iunit,irep) = 0.0
+            flg_all = .False.
         else
            i1 = lunit2mp(1, iunit)
            i2 = lunit2mp(2, iunit)
@@ -52,6 +45,18 @@ subroutine simu_rmsd(rmsd_unit, rmsd)
                 list1, list2, rmsd_unit(iunit, irep))
         endif
      end do
+
+     if (flg_all) then
+        i1 = 1
+        i2 = nmp_real
+        num = i2 - i1 + 1
+        xyz_init_mp(1:3, i1:i2) = xyz_ref_mp(1:3, i1:i2)
+        xyz_in_mp(1:3, i1:i2) = xyz_mp_rep(1:3, i1:i2, irep)
+        call util_bestfit(xyz_init_mp, num, xyz_in_mp, num, num, xyz_out_mp, &
+             list1, list2, rmsd(irep))
+     else
+        rmsd(irep) = 0.0
+     endif
 
   end do
 
